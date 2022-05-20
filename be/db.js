898a669493da85a75e59763eb2d90c7ead8ae512
jemma <ejayjeon@gmail.com>
2022-05-20 20:56:51 +0900

    be & fe dockerfile

--- be/db.js
+++ be/db.js
@@ -1,31 +1,29 @@
-const { text } = require("body-parser");
 const mysql = require("mysql");
 require("dotenv").config();
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PWD,
-//   database: process.env.DB_NAME,
-// });
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
 });
-connection.connect();
-const list = connection.query(
-  `CREATE TABLE lists(
-    id INTEGER AUTO INCREMENT;
-    value TEXT;
-    PRIMARY KEY(id);
-  )`,
-  (err, results) => {
-    console.log(`results: ` + results);
-  }
-);
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PWD,
+//   database: process.env.DB_NAME,
+// });
+// connection.connect();
+// const list = connection.query(
+//   `CREATE TABLE lists(
+//     id INTEGER AUTO INCREMENT;
+//     value TEXT;
+//     PRIMARY KEY(id);
+//   )`,
+//   (err, results) => {
+//     console.log(`results: ` + results);
+//   }
+// );
 
-exports.connection = connection;
-exports.list = list;
+exports.pool = pool;
+// exports.list = list;
--- be/server.js
+++ be/server.js
@@ -1,28 +1,64 @@
+// 필요한 모듈 가져오기
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+// Express 서버 생성
 const app = express();
-const db = require("./db");
+
+// JSON 형태로 오는 요청의 본문을 해석
 app.use(bodyParser.json());
 
-// 테이블 생성
-var list = db.list;
-app.get("/", (req, res) => {
-  db.connection.query(`SELECT * FROM ${list};`, (err, results) => {
-    if (err) return res.json(`결과: ${results}`);
-    else return res.json(results);
+// Db 가져와서 연결하기
+const db = require("./db");
+
+// DB 테이블 생성
+db.pool.query(
+  `CREATE TABLE lists(
+  id INTEGER AUTO_INCREMENT,
+  value TEXT,
+  PRIMARY KEY (id)
+)`,
+  (err, result) => {
+    console.log(`result: ${result}`);
+  }
+);
+
+// DB lists 테이블에 있는 모든 데이터를 프론트에 보여주기
+app.get("/api/values", (req, res) => {
+  db.pool.query(`SELECT * FROM lists;`, (err, result) => {
+    if (err) return res.status(500).send(err);
+    else return res.json(result);
   });
 });
 
-// Client에서 입력한 데이터를 DB에 넣어주기
+// 클라이언트에서 입력한 데이터를 DB lists에 넣어주기
 app.post("/api/value", (req, res) => {
-  db.connection.query(
-    `INSERT INTO ${list} (value) VALUES("${req.body.value}")`,
-    (err, results) => {
-      if (err) return res.send(err);
+  db.pool.query(
+    `INSERT INTO lists (value) VALUES("${req.body.value}")`,
+    (err, result) => {
+      if (err) return res.status(500).send(err);
       else return res.json({ success: true, value: req.body.value });
     }
   );
 });
+
+// var list = db.list;
+// app.get("/", (req, res) => {
+//   db.connection.query(`SELECT * FROM ${list};`, (err, results) => {
+//     if (err) return res.json(`결과: ${results}`);
+//     else return res.json(results);
+//   });
+// });
+
+// Client에서 입력한 데이터를 DB에 넣어주기
+// app.post("/api/value", (req, res) => {
+//   db.connection.query(
+//     `INSERT INTO ${list} (value) VALUES("${req.body.value}")`,
+//     (err, results) => {
+//       if (err) return res.send(err);
+//       else return res.json({ success: true, value: req.body.value });
+//     }
+//   );
+// });
 
 app.listen(process.env.PORT, () => console.log("listing on PORT"));

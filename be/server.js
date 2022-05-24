f98ece2c43228111fef3d89e531bbd03c7433494
jemma <ejayjeon@gmail.com>
2022-05-24 19:36:03 +0900

    docker-compose

--- be/server.js
+++ be/server.js
@@ -12,16 +12,16 @@ app.use(bodyParser.json());
 const db = require("./db");
 
 // DB 테이블 생성
-db.pool.query(
-  `CREATE TABLE lists(
-  id INTEGER AUTO_INCREMENT,
-  value TEXT,
-  PRIMARY KEY (id)
-)`,
-  (err, result) => {
-    console.log(`result: ${result}`);
-  }
-);
+// db.pool.query(
+//   `CREATE TABLE lists(
+//   id INTEGER AUTO_INCREMENT,
+//   value TEXT,
+//   PRIMARY KEY (id)
+// )`,
+//   (err, result) => {
+//     console.log(`result: ${result}`);
+//   }
+// );
 
 // DB lists 테이블에 있는 모든 데이터를 프론트에 보여주기
 app.get("/api/values", (req, res) => {
@@ -41,24 +41,5 @@ app.post("/api/value", (req, res) => {
     }
   );
 });
-
-// var list = db.list;
-// app.get("/", (req, res) => {
-//   db.connection.query(`SELECT * FROM ${list};`, (err, results) => {
-//     if (err) return res.json(`결과: ${results}`);
-//     else return res.json(results);
-//   });
-// });
-
-// Client에서 입력한 데이터를 DB에 넣어주기
-// app.post("/api/value", (req, res) => {
-//   db.connection.query(
-//     `INSERT INTO ${list} (value) VALUES("${req.body.value}")`,
-//     (err, results) => {
-//       if (err) return res.send(err);
-//       else return res.json({ success: true, value: req.body.value });
-//     }
-//   );
-// });
 
 app.listen(process.env.PORT, () => console.log("listing on PORT"));
